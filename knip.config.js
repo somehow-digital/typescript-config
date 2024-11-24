91f84ca156d7b77dc1f263ef291a2886267bc54a
Thomas Deinhamer <code@thasmo.dev>
2024-11-24 15:38:42 +0100

    Update knip configuration

--- knip.config.js
+++ knip.config.js
@@ -1,4 +1,10 @@
 export default {
 	entry: ['configuration/*.js'],
+	ignoreDependencies: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/github',
+		'@semantic-release/npm',
+		'@semantic-release/release-notes-generator',
+	],
 	project: ['configuration/**'],
 };
