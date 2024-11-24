f44cb247a830392c37fa8d3095b586bb6297ff16
Thomas Deinhamer <code@thasmo.dev>
2024-11-24 15:14:33 +0100

    Add semantic release workflow

---
+++ release.config.js
@@ -0,0 +1,9 @@
+export default {
+	branches: ['main'],
+	plugins: [
+		'@semantic-release/commit-analyzer',
+		'@semantic-release/release-notes-generator',
+		'@semantic-release/github',
+		'@semantic-release/npm',
+	],
+};
