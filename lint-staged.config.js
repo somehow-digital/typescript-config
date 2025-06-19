8ae49a9a497227826655d5b1da2839e3c555e83a
Thomas Deinhamer <code@thasmo.dev>
2025-06-19 22:34:32 +0200

    Fix linting issues

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,4 @@
 export default {
 	'**/*.{js,jsx,ts,tsx}': ['eslint'],
-	'**/*.{md,json,yml,yaml}': [
-		'prettier --check',
-		'cspell --no-progress --no-must-find-files',
-	],
+	'**/*.{md,json,yml,yaml}': ['prettier --check', 'cspell --no-progress --no-must-find-files'],
 };
